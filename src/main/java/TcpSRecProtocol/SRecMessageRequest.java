e95e5a5d64add020cadc8976635212028119ed57
Jose Zamora <europeoskater@gmail.com>
2019-09-12 22:13:48 +0200

    Added comments to all Messages

--- src/main/java/TcpSRecProtocol/SRecMessageRequest.java
+++ src/main/java/TcpSRecProtocol/SRecMessageRequest.java
@@ -2,15 +2,31 @@ package TcpSRecProtocol;
 
 import java.io.File;
 
+/**
+ * @author Jose Miguel Zamora Batista.
+ * SRecMessage implementation,
+ * This class is used for send messages
+ * from client to server.
+ */
 public class SRecMessageRequest extends SRecMessage {
 
+    /* File to send */
     private final File file;
 
+    /**
+     * Constructor of a request.
+     * @param code the code of the new request
+     * @param file the file to send. Can be Null.
+     */
     public SRecMessageRequest(byte code, File file) {
         super(code);
         this.file = file;
     }
 
+    /**
+     * Return the file.
+     * @return the file.
+     */
     public File getFile() {
         return file;
     }
--- src/main/java/TcpSRecProtocol/SRecMessageResponse.java
+++ src/main/java/TcpSRecProtocol/SRecMessageResponse.java
@@ -1,7 +1,18 @@
 package TcpSRecProtocol;
 
+
+/**
+ * @author Jose Miguel Zamora Batista.
+ * SRecMessage implementation,
+ * This class is used for send messages
+ * from server to clients.
+ */
 public class SRecMessageResponse extends SRecMessage{
 
+    /**
+     * Constructor of a response.
+     * @param code the code of the new response.
+     */
     public SRecMessageResponse(byte code) {
         super(code);
     }
